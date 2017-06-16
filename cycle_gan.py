cc90e2767ff3c7745aed3913ed47ea867e120b2e
Dmitry Kostyaev <d.kostyaev@babo.com>
2017-06-16 12:36:52 +0300

    clip image

--- cycle_gan.py
+++ cycle_gan.py
@@ -5,7 +5,8 @@ from networks import *
 def convert2int(image):
   """ Transfrom from float tensor ([-1.,1.]) to int image ([0,255])
   """
-  return tf.image.convert_image_dtype((image+1.0)/2.0, tf.uint8)
+  img = (image+1.0)/2.0
+  return tf.image.convert_image_dtype(tf.clip_by_value(img, 0, 255), tf.uint8)
 
 def convert2float(image):
   """ Transfrom from int image ([0,255]) to float tensor ([-1.,1.])
