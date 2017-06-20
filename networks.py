46a7a1de5ebf4cf42dfbe97e1c775158c6062b0e
Dmitry Kostyaev <d.kostyaev@babo.com>
2017-06-20 10:47:51 +0300

    add additional ops

--- networks.py
+++ networks.py
@@ -94,7 +94,7 @@ class Generator:
             x = conv2d_transpose(x, self.ngf, 3, 2, name='g_ct2')
             x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], "REFLECT")
             x = conv2d_simple(x, 3, 5, 1, padding='VALID', activation_fn=self.activation)
-            if not activation:
+            if not self.activation:
                 x = tf.clip_by_value(x, -1.0, 1.0)
 
         self.reuse = True
